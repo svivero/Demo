f1b02b36e9134eb244f09d90493a26977f3f7654
Steven Vivero <svivero@gmail.com>
2017-10-29 13:48:45 +1100

    f,r(routes): Add nav to route styleguide content

--- Demo.Web.Styleguide/angular4/src/app/app.module.ts
+++ Demo.Web.Styleguide/angular4/src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule }      from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Registers critical application service providers, it also includes common directives like NgIf
 import { BrowserModule } from '@angular/platform-browser';
 
 //
 // Components
 //
-import { SgHeaderComponent }  from './styleguide/header/sg-header.component';
+import { SgHeaderComponent }  from './styleguide/components/header/sg-header.component';
+import { SgButtonsComponent }  from './styleguide/components/elements/sg-buttons.component';
+import { AppComponent } from './styleguide/components/app.component';
+
+export const ROUTES: Routes = [
+  { path: '', pathMatch: 'full', component: AppComponent },
+  { path: 'elements/buttons', component: SgButtonsComponent }
+];
 
 //
 // Services
 //
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ SgHeaderComponent ],
-  bootstrap:    [ SgHeaderComponent ],
+  imports:      [
+    BrowserModule,
+
+    // Define root config for the main module
+    RouterModule.forRoot(ROUTES)
+  ],
+  declarations: [
+    SgHeaderComponent,
+    SgButtonsComponent,
+    AppComponent
+  ],
+
+  // Load these components on app start up
+  bootstrap:    [
+    SgHeaderComponent,
+    AppComponent
+  ],
 })
 export class AppModule { }
---
+++ Demo.Web.Styleguide/angular4/src/app/styleguide/components/elements/sg-buttons.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sg-buttons',
+  templateUrl: './sg-buttons.component.html',
+})
+export class SgButtonsComponent  {
+}
