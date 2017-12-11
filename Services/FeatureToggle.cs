9da3e519c73aa2bbb04679b0c8c6d76452529160
Steven Vivero <svivero@gmail.com>
2017-12-11 21:10:37 +1100

    feat(*): add feature toggle

---
+++ Services/FeatureToggle.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+
+namespace Services
+{
+    public class FeatureToggle
+    {
+        public bool IsEnabled(string name)
+        {
+            // default to enabled if setting not found or not boolean
+            var settingValue = true;
+
+            if (bool.TryParse(ConfigurationManager.AppSettings[name], out settingValue))
+            {
+                return settingValue;
+            }
+            return settingValue;
+        }
+    }
+}
